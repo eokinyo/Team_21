92f867dbc03b591fbef9a67f3b6a60693bf73aa3
eokinyo <50834185+eokinyo@users.noreply.github.com>
2024-02-15 10:27:22 +0200

    Update ellydb.php

--- ellySwork/ellydb.php
+++ ellySwork/ellydb.php
@@ -1,7 +1,5 @@
 <?php 
 $title = "User data";
-include_once 'outline/header.php'; ?>
-<?php
 $servername = "web-dev-env-main-db-1";
 $username = "team21";
 $password = "@Jan302024";
@@ -17,4 +15,3 @@ if($conn->connect_error){
     die("Connection Failed: " . $conn->connect_error);
 }
 ?>
-<?php include_once '../outline/footer.php'; ?>
